00cfa2cad458febabadcb9e4a6434ff371310643
unknown <1136592005@qq.com>
2020-12-16 09:51:36 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import echarts from 'echarts'
+import axios from 'axios'
 
+// 使用elementUI
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   configureWebpack: {
+//     resolve: {
+//       // 配置文件的别名
+//       alias: {
+//         'assets': '@/assets',
+//         'common': '@/common',
+//         'components': '@/components',
+//         'network': '@/network',
+//         'views': '@/views',
+//       }
+//     }
+//   },
+//   devServer: {
+//     post: 8080,
+//     host: 'localhost',
+//     open: ture //配置浏览器自动打开
+//   }
+// }
+
